996fc467c7a70da31c1dacc73a9b953a9a6d8fdb
dactrong <trongndph13723@fpt.edu.vn>
2022-05-13 08:57:27 +0700

    feat:update

--- src/app/identity/identity.component.ts
+++ src/app/identity/identity.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-identity',
@@ -6,6 +6,7 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./identity.component.css']
 })
 export class IdentityComponent implements OnInit {
+  @Input () identity: any;
 
   constructor() { }
 
